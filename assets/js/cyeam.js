4ab33c020d17e10107438bb65fd15afc37315e12
Bryce <lichao0407@gmail.com>
2014-04-25 14:43:48 +0800

    bing wallpaper

--- assets/js/cyeam.js
+++ assets/js/cyeam.js
@@ -1,6 +1,25 @@
 function doodle() {
 	$.getJSON("http://doodle.cyeam.com/js?jsoncallback=?", function(json) {
-		console.debug(json)
 		$("body").css("background", "url(" + json[0].doodle + ") center no-repeat fixed");
 	});
+}
+
+function getDoodle(doodle_container, title_container) {
+	$.getJSON("http://doodle.cyeam.com/js?jsoncallback=?", function(json) {
+		document.getElementById(doodle_container).src = json[0].doodle;
+		document.getElementById(title_container).innerHTML = json[0].title;
+		// $("#" + container).css("src", json[0].doodle);
+	});
+}
+
+function bing(container) {
+	if (container != null) {
+		$.getJSON("http://bing.cyeam.com/js?jsoncallback=?", function(json) {
+			$("#" + container).css("background", "url(" + json[0].bing + ") no-repeat center center");
+		});
+	} else {
+		$.getJSON("http://bing.cyeam.com/js?jsoncallback=?", function(json) {
+			$("body").css("background", "url(" + json[0].bing + ") no-repeat center center fixed");
+		});
+	}
 }
