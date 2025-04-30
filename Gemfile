ccc6fa98486a01064373935305bf4e6a168f76b3
mnhkahn <lichao@cyeam.com>
2025-04-30 18:41:47 +0800

    feat: 添加图片放大功能并清理冗余代码
    - 在 `style.css` 中添加 `.enlarged` 类，用于图片放大效果
    - 在 `cyeam.js` 中实现图片点击放大功能，并移除未使用的代码
    - 清理 `post.html` 和 `default.html` 中的冗余脚本
    - 添加 `Gemfile` 和 `Gemfile.lock` 以管理依赖

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem 'nokogiri'
+gem 'kramdown-parser-gfm'
+gem 'jekyll-watch'
+gem 'webrick'
--- assets/js/cyeam.js
+++ assets/js/cyeam.js
@@ -1,34 +1,12 @@
-function doodle() {
-	$.getJSON("http://doodle.cyeam.com/js?jsoncallback=?", function(json) {
-		$("body").css("background", "url(" + json[0].doodle + ") center no-repeat fixed");
-	});
-}
-
-function getDoodle(doodle_container, title_container) {
-	$.getJSON("http://doodle.cyeam.com/js?jsoncallback=?", function(json) {
-		document.getElementById(doodle_container).src = json[0].doodle;
-		document.getElementById(title_container).innerHTML = json[0].title;
-		// $("#" + container).css("src", json[0].doodle);
-	});
-}
-
-function bing(container) {
-	if (container != null) {
-		//$("#" + container).css("background", "url(http://chatianqi.net/bingImg/)  no-repeat center center fixed");
-		// $.getJSON("http://bing.cyeam.com/js?jsoncallback=?", function(json) {
-		// 	$("#" + container).css("background", "url(" + json[0].bing + ") no-repeat center center");
-		// });
-	} else {
-		// $.getJSON("http://bing.cyeam.com/js?jsoncallback=?", function(json) {
-		// 	$("body").css("background", "url(" + json[0].bing + ") no-repeat center center fixed");
-		// });
-		//$("body").css("background", "url(http://chatianqi.net/bingImg/) no-repeat center center fixed");
+$(document).ready(function () {
+	// 获取所有 img 标签
+	const images = document.getElementsByTagName('img');
+	// 遍历 img 标签并添加 onclick 方法
+	for (let i = 0; i < images.length; i++) {
+		images[i].onclick = function () {
+			// alert('你点击了图片');
+			var image = images[i];
+			image.classList.toggle('enlarged');
+		};
 	}
-}
-
-function wallpaper() {
-//	$.getJSON("http://lab.cyeam.com/ip?jsoncallback=?", function (json) {
- //   if (json[0].useragent.IsMobile == false) {
-        bing()
-//});
-}
+});
